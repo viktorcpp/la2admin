2d7153e148fbd8f499088cb46082bbe2eb9dbb99
viktorcpp <viktor.cpp.root@gmail.com>
2024-08-10 10:05:26 +0300

    items icons fix

--- src/app/components/rb-info-item.js
+++ src/app/components/rb-info-item.js
@@ -11,7 +11,7 @@ export default function RBInfoItem(props) {
                     <img src={"/icons/" + item.iconName + ".png"} alt="" className="rbinfo__ico" width="32" height="32"/>
                     {item.itemName}
                 </Link>
-                <span className="rbinfo__minmax">{item.dropMin + " - " + item.dropMax}</span>
+                <span className="rbinfo__minmax">{item.dropMin + "-" + item.dropMax}</span>
                 <span className="rbinfo__chance">{ (item.chance/10000).toFixed(2) }<sub>%</sub></span>
             </div>
         )
