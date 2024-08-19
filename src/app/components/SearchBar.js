b27d32784242bc7c5ad662d132982d13ff331698
viktorcpp <viktor.cpp.root@gmail.com>
2024-08-19 09:24:17 +0300

    + btn states medium size

--- src/app/components/SearchBar.js
+++ src/app/components/SearchBar.js
@@ -1,14 +1,29 @@
 
+import { FormEvent } from 'react';
+
 export default function SearchBar() {
+
+    function handleSubmit(event) {
+
+        const formData = new FormData(event.currentTarget);
+
+        if( formData.get("search") == '' )
+            event.preventDefault();
+    }
+
     return (
         <div className="searchbar">
-            <form action="/main/search" method="get">
+            <form action="/main/search" method="get" onSubmit={handleSubmit}>
                 <div className="searchbar__row">
                     <div className="form-item form-item-search">
                         <input type="search" name="search" placeholder="Enter text here"/>
                     </div>
                     <div className="form-item form-item-submit">
-                        <input type="submit"/>
+                        <div className="form-submit30__left">
+                            <div className="form-submit30__right">
+                                <input type="submit"/>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </form>
